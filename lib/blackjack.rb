f71a6458f19ee56ee0fe572768e470ed187995b1
Megan Farley <megfarley4@gmail.com>
2018-10-03 02:56:18 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,11 +62,11 @@ end
 
 def runner
   # code runner here
-#  welcome
- # sum = initial_round
-#  while sum <= 21
-#    sum += hit?(sum)
- # end
-#end_game(sum)
+  welcome
+  sum = initial_round
+  while sum <= 21
+    sum += hit?(sum)
+  end
+end_game(sum)
 end
     
